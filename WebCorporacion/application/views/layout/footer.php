f64c4d7b0530b50bbe1309f7b8a39f333e46c6da
Herberd <jesuss1201@gmail.com>
2018-02-02 12:49:08 -0500

    agregando stilo

--- WebCorporacion/application/views/layout/footer.php
+++ WebCorporacion/application/views/layout/footer.php
@@ -1,125 +1,198 @@
-<footer class="footer footer5">
+<footer class="footer">
 	<!-- End .footer-newsletter -->
-	<div class="footer-inner">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 col-md-3">
-					<div class="widget">
-						<h4 class="widget-title">Information</h4>
-						<ul class="links">
-							<li><a href="#">New products</a></li>
-							<li><a href="#">Top sellers</a></li>
-							<li><a href="#">Specials products</a></li>
-							<li><a href="#">Manufacturers</a></li>
-							<li><a href="#">Suppliers</a></li>
-							<li><a href="#">Our Stores</a></li>
-							<li><a href="#">Coupons</a></li>
-						</ul>
-					</div>
-					<!-- End .widget -->
-				</div>
-				<!-- End .col-md-3 -->
+	
+                <div class="footer-inner">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-5 col-sm-4">
+                                <div class="widget about-widget">
+                                    <img src="<?=base_url()?>public/assets/images/footer-logo.png" class="footer-logo" alt="Simple Footer Logo">
+                                    <p>Tu tienda online! 100% Especializados en repuestos para todos los electrodomésticos y prácticamente todas las marcas.</p>
+                                    <p>La forma fácil de encontrar y comprar repuestos originales para electrodomésticos.</p>
+                                </div><!-- End .widget -->
+                            </div><!-- End .col-5 -->
 
-				<div class="col-sm-6 col-md-3">
-					<div class="widget">
-						<h4 class="widget-title">Customer Service</h4>
-						<ul class="links">
-							<li><a href="#">Help &amp; Contact</a></li>
-							<li><a href="#">Shipping &amp; Taxes</a></li>
-							<li><a href="#">Return Policy</a></li>
-							<li><a href="#">Careers</a></li>
-							<li><a href="#">Affiliates</a></li>
-							<li><a href="#">Legal Notice</a></li>
-							<li><a href="#">Privacy Policy</a></li>
-						</ul>
-					</div>
-					<!-- End .widget -->
-				</div>
-				<!-- End .col-md-3 -->
+                           <div class="col-5 col-sm-4">
+                                <div class="widget">
+                                    <h4 class="widget-title">Información</h4>
+                                    <ul class="links">
+                                        <li><a href="#">Nuevos Productos</a></li>
+                                        <li><a href="#">Los más vendidos</a></li>
+                                        <li><a href="#">Productos especiales</a></li>
+                                        <li><a href="#">Fabricantes</a></li>
+                                        <li><a href="#">Proveedores</a></li>
+                                        <li><a href="#">Nuestras tiendas</a></li>
+                                    </ul>
+                                </div><!-- End .widget -->
+                            </div><!-- End .col-5 -->
 
-				<div class="col-sm-6 col-md-3">
-					<div class="widget">
-						<h4 class="widget-title">My Account</h4>
-						<ul class="links">
-							<li><a href="#">My Account</a></li>
-							<li><a href="#">Personal Information</a></li>
-							<li><a href="#">Payment Methods</a></li>
-							<li><a href="#">Discount</a></li>
-							<li><a href="#">Orders History</a></li>
-							<li><a href="#">Your Vouchers</a></li>
-							<li><a href="#">Addresses</a></li>
-						</ul>
-					</div>
-					<!-- End .widget -->
-				</div>
-				<!-- End .col-md-3 -->
+                            <div class="col-5 col-sm-4">
+                                <div class="widget">
+                                    <h4 class="widget-title">Servicio al cliente</h4>
+                                    <ul class="links">
+                                         <li><a href="#">Solicitar ayuda</a></li>
+                                        <li><a href="#">Envío e Impuestos</a></li>
+                                        <li><a href="#">Política de devolución</a></li>
+                                        <li><a href="#">Fabricantes</a></li>
+                                        <li><a href="#">Afiliados</a></li>
+                                        <li><a href="#">Aviso legal</a></li>
+                                    </ul>
+                                </div><!-- End .widget -->
+                            </div><!-- End .col-5 -->
 
-				<div class="col-sm-6 col-md-3">
-					<div class="widget">
-						<h4 class="widget-title">Soporte t&eacute;cnico</h4>
+                            <div class="col-5 col-sm-4">
+                                <div class="widget">
+                                    <h4 class="widget-title">Mi Cuenta</h4>
+                                    <ul class="links">
+                                        <li><a href="#">Ingresar</a></li>
+                                        <li><a href="#">Direcciones</a></li>
+                                        <li><a href="#">Descuentos</a></li>
+                                        <li><a href="#">Historial de Pedidos</a></li>
+                                        <li><a href="#">Sus Cupones</a></li>
+                                    </ul>
+                                </div><!-- End .widget -->
+                            </div><!-- End .col-5 -->
 
-						<ul class="call-list">
-							<li>Nextel: 412*5116</li>
-							<li>Fijo: 01 242 4595</li>
-							<li>Celular: 994 125 116</li>
-						</ul>
-						<p>Email: soporte@corporacionderepuestos.com</p>
-					</div>
-					<!-- End .widget -->
+                            <div class="col-5 col-sm-4">
+                                <div class="widget">
+                                  <h4 class="widget-title">Detalles de contacto</h4>
+                                    <ul class="contact-list">
+                                        <li>
+                                            <i class="icon info-icon phone"></i>
+                                            
+                                            <div>968-990-826</div>
+                                            <div>968-990-853</div>
+                                        </li>
+                                        <li>
+                                            <i class="icon info-icon mobile"></i>
+                                            
+                                            <div>51 01 242-4595</div>
+                                            <div>51 01 426-0264</div>
+                                        </li>
+                                        <li>
+                                            <i class="icon info-icon email"></i>
+                                            <div><a href="mailto:#">ventas@corporacionderepuestos.com</a></div>
+                                            <div><a href="mailto:#">servicios@corporacionderepuestos.com</a></div>
+                                            
+                                        </li>
+                                        <li>
+                                            <i class="icon info-icon skype"></i>
+                                            <div>ventas_contacto</div>
+                                            <div>servicios_contacto</div>
+                                            
+                                        </li>
+                                    </ul>
+                                </div><!-- End .widget -->
+                            </div><!-- End .col-5 -->
+                        </div><!-- End .row -->
+                    </div><!-- End .container-fluid -->
+                </div><!-- End .footer-inner -->
 
-					<div class="widget">
-						<h4 class="widget-title">Follow Us</h4>
+                <div class="footer-bottom">
+                    <div class="container-fluid">
+                        <div class="footer-left">
+                            <div class="social-icons">
+                                <a href="#" class="social-icon icon facebook" title="Facebook"><span class="sr-only">Facebook</span></a>
+                                <a href="#" class="social-icon icon twitter" title="Twitter"><span class="sr-only">Twitter</span></a>
+                                <a href="#" class="social-icon icon pinterest" title="Pinterest"><span class="sr-only">Pinterest</span></a>
+                                <a href="#" class="social-icon icon instagram" title="Instagram"><span class="sr-only">Instagram</span></a>
+                                <a href="#" class="social-icon icon flickr" title="Flickr"><span class="sr-only">Flickr</span></a>
+                                <a href="#" class="social-icon icon skype" title="Skype"><span class="sr-only">Skype</span></a>
+                                <a href="mailto:#" class="social-icon icon email" title="Email"><span class="sr-only">Email</span></a>
+                            </div><!-- End .social-icons -->
+                            <ul class="footer-menu">
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Information</a></li>
+                                <li><a href="#">Terms &amp; Conditions</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Sitemap</a></li>
+                                <li><a href="#">Free Delivery</a></li>
+                                <li><a href="#">Support</a></li>
+                            </ul>
+                        </div><!-- End .footer-right -->
 
-						<div class="social-icons smaller">
-							<a href="#" class="social-icon icon facebook" title="Facebook"><span
-								class="sr-only">Facebook</span></a> <a href="#"
-								class="social-icon icon twitter" title="Twitter"><span
-								class="sr-only">Twitter</span></a> <a href="#"
-								class="social-icon icon pinterest" title="Pinterest"><span
-								class="sr-only">Pinterest</span></a> <a href="#"
-								class="social-icon icon instagram" title="Instagram"><span
-								class="sr-only">Instagram</span></a> <a href="#"
-								class="social-icon icon flickr" title="Flickr"><span
-								class="sr-only">Flickr</span></a> <a href="#"
-								class="social-icon icon skype" title="Skype"><span
-								class="sr-only">Skype</span></a> <a href="mailto:#"
-								class="social-icon icon email" title="Email"><span
-								class="sr-only">Email</span></a>
-						</div>
-						<!-- End .social-icons -->
-					</div>
-					<!-- End .widget -->
-				</div>
-				<!-- End .col-md-3 -->
+                        <div class="footer-right">
+                            <div class="payment-info">
+                                <h5>100% Secure and Trusted Payment</h5>
+                                <p>Easy returns. Free shipping on orders over $100. Need help? <a href="#">Help Center.</a></p>
+                                <img src="assets/images/cards.png" alt="Payment methods">
+                            </div><!-- End .payment-info -->
+                        </div><!-- End .footer-right -->
 
-			</div>
-			<!-- End .row -->
-		</div>
-		<!-- End .container -->
-	</div>
-	<!-- End .footer-inner -->
+                        <a class="scroll-top icon" href="#top" title="Scroll top"><span class="sr-only">Scroll Top</span></a>
+                    </div><!-- End .container-fluid -->
+                </div><!-- End .footer-bottom -->
+            </footer><!-- End .footer -->
 
-	<div class="footer-bottom text-center">
-		<div class="container">
-			<img src="<?=base_url()?>public/assets/images/footer-logo2.png" alt="Sconto"
-				class="footer-logo">
-			<div class="payment-info">
-				<p>
-					Easy returns. Free shipping on orders over $100. Need help? <a
-						href="#">Help Center.</a>
-				</p>
-				<img src="<?=base_url()?>public/assets/images/cards.png" alt="Payment methods">
-			</div>
-			<!-- End .payment-info -->
+       
 
-			<a class="scroll-top icon" href="#top" title="Scroll top"><span
-				class="sr-only">Scroll Top</span></a>
-		</div>
-		<!-- End .container -->
-	</div>
-	<!-- End .footer-bottom -->
-</footer>
-<!-- End .footer -->
+        <!-- End -->
+        <script src="assets/js/plugins.min.js"></script>
+        <script src="assets/js/main.js"></script>
+
+        <!-- REVOLUTION JS FILES -->
+        <script type="text/javascript" src="assets/js/jquery.themepunch.tools.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.themepunch.revolution.min.js"></script>
+
+        <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+        (Load Extensions only on Local File Systems !  
+        The following part can be removed on Server for On Demand Loading) -->  
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.actions.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.carousel.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.migration.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.parallax.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script type="text/javascript" src="assets/js/extensions/revolution.extension.video.min.js"></script>
+
+        <script type="text/javascript">
+            (function() {
+                "use strict";
+
+                var tpj=jQuery, revapi20;                 
+                tpj(document).ready(function() {
+                    if(tpj("#rev_slider_20_1").revolution == undefined) {
+                        revslider_showDoubleJqueryError("#rev_slider_20_1");
+                    } else {
+                        revapi20 = tpj("#rev_slider_20_1").show().revolution({
+                            sliderType:"hero",
+                            jsFileLocation:"assets/js/",
+                            sliderLayout:"auto",
+                            dottedOverlay:"none",
+                            delay:9000,
+                            navigation: {
+                            },
+                            responsiveLevels:[1200,1024,778,480],
+                            gridwidth:[1170,1024,778,480],
+                            gridheight:[800,720,680,600],
+                            lazyType:"single",
+                            parallax: {
+                                type:"mouse",
+                                origo:"slidercenter",
+                                speed:2000,
+                                levels:[2,3,4,5,6,7,12,16,10,50],
+                            },
+                            shadow:0,
+                            spinner:"off",
+                            autoHeight:"off",
+                            disableProgressBar:"on",
+                            hideThumbsOnMobile:"off",
+                            hideSliderAtLimit:0,
+                            hideCaptionAtLimit:0,
+                            hideAllCaptionAtLilmit:0,
+                            debugMode:false,
+                            fallbacks: {
+                                simplifyAll:"off",
+                                disableFocusListener:false,
+                            }
+                        });
+                    }
+                });
+
+            })();
+        </script>
 
 <script src="<?=base_url()?>public/assets/js/plugins.min.js"></script>
     <script src="<?=base_url()?>public/assets/js/main.js"></script>
